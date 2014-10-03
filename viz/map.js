6f2af5d8281fe9f713ff4bbaa446511684fbd218
Luis Capelo <luiscape@gmail.com>
2014-10-03 15:43:43 -0400

    fixing tooltip

--- viz/map.js
+++ viz/map.js
@@ -1,4 +1,12 @@
 /// adding mapbox map
 L.mapbox.accessToken = 'pk.eyJ1IjoibHVpc2NhcGVsbyIsImEiOiJZeFo3b0pNIn0.45gYdbaSP04kpEIuLwSA9w';
-var map = L.mapbox.map('map', 'luiscapelo.jlpda14n')
-    .setView([5, -65], 6);
+var map = L.mapbox.map('map', 'luiscapelo.jlpda14n', {
+        attributionControl: false,
+        infoControl:true
+    });
+
+// for the tooptip ti follow cursor
+map.gridControl.options.follow = true;
+
+// for setting the center point
+map.setView([5, -65], 6);
